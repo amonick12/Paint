d488dc5c1d8165968bd69c74738e63b3d326a66f
Aaron Monick <amonick12@gmail.com>
2014-09-25 14:10:46 -0400

    myMenu created

--- Paint/src/paint/DrawingBoard.java
+++ Paint/src/paint/DrawingBoard.java
@@ -6,18 +6,36 @@
 
 package paint;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
  *
  * @author aaronmonick
  */
-public class DrawingBoard extends JPanel {
+public class DrawingBoard extends JPanel implements ActionListener {
+    
     myMenu menu;
+    JButton eraserButton;
+    boolean eraser;
+    //Shapes shape;
+    //Colors color;
+    //BrushSize size;
+    int index;
+    
+    
    
     public DrawingBoard(){
         super();
         menu = new myMenu();
+        add(menu);
     } 
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
